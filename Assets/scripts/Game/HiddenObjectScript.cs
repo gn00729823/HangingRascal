0cf8fbeb0ee13d7a6fd3fe9b0d84e07804d82983
samuellin <samuellin@jumbogames.com.tw>
2017-04-25 16:11:15 +0800

    ServerMessage

--- Assets/scripts/Game/HiddenObjectScript.cs
+++ Assets/scripts/Game/HiddenObjectScript.cs
@@ -29,19 +29,6 @@ public class HiddenObjectScript : MonoBehaviour {
 		return result;
 	}
 
-//	void OnTriggerEnter2D(Collider2D collider){
-//		if (collider.gameObject.tag == "MainPlayer") {
-//			Material mat = gameObject.GetComponent<MeshRenderer> ().material;
-//			mat.color = new Color (mat.color.r, mat.color.g, mat.color.b, 0.5f);
-//		}
-//	}
-//	void OnTriggerExit2D(Collider2D collider){
-//		if (collider.gameObject.tag == "MainPlayer") {
-//			Material mat = gameObject.GetComponent<MeshRenderer> ().material;
-//			mat.color = new Color (mat.color.r, mat.color.g, mat.color.b, 1);
-//		}
-//	}
-
 
 }
 	
