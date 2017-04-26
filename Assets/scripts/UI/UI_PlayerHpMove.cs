4fac621ab7447b73753f021be523ed614e54653a
JUMBO\jimchu <jimchu@jumbogames.com.tw>
2017-04-26 14:04:44 +0800

    角色血條實作

---
+++ Assets/scripts/UI/UI_PlayerHpMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class UI_PlayerHpMove : MonoBehaviour {
+    private GameObject hp_obj ;
+    private float MaxPosX = -3.4f;
+    private float MixPosX = -91f;
+    private float OneChagePosX;
+    private BattleSpriteAction myTarget;
+	// Use this for initialization
+    void Awake()
+    {
+        myTarget = transform.parent.parent.gameObject.GetComponent<BattleSpriteAction>();
+
+        if(!myTarget.isMainPlayer)
+        {
+            Destroy(this.gameObject);
+        }
+        hp_obj = this.transform.FindChild("_hp").gameObject;
+        OneChagePosX = (Mathf.Abs(MixPosX) - Mathf.Abs(MaxPosX))/100;
+        
+      
+    }
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hp_obj.transform.localPosition = new Vector3(MixPosX + (myTarget.hp*OneChagePosX),0.4f,0);
+	}
+}
